d96eba2a2a4f688227f8552738bff8fe47f6267c
Kristel Teng <kristelteng@gmail.com>
2017-05-02 15:21:29 -0700

    add admin model

--- play/polls/admin.py
+++ play/polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from polls.models import Question
 # Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+  list_display = ('question_text', 'pub_date', 'was_published_recently' )
+
+admin.site.register(Question, QuestionAdmin)
